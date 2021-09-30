c13f62c1c9fae9f055d60c7da6a662c489e27106
pawelhenryk <pawelwos90@gmail.com>
2021-09-30 11:07:24 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <v-row>
       <v-card>
         <div id="auth" class="noprint"></div>
-        <div id="app" data-spa="1" data-ptoken="bfe64a2de784492683724b295c7e5d11" data-lang="pl" data-env="prod" ></div>
+        <div id="app" data-spa="1" data-ptoken="bfe64a2de784492683724b295c7e5d11" data-lang="pl" data-env="local" ></div>
         <script src="https://octobus.cloud/frameapp/js/manifest.js"></script>
         <script src="https://octobus.cloud/frameapp/js/vendor.js"></script>
         <script src="https://octobus.cloud/frameapp/js/app.js"></script>
